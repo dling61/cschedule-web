070be347c774a09476b39e711d84ccfd22489a75
littlewormQ <2422915252@qq.com>
2014-01-22 10:04:06 +0800

    'Chinese characters' issue
    'Chinese characters' issue

--- protected/views/member/mycontacts.php
+++ protected/views/member/mycontacts.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>My Contacts</title>
 <link href="./css/cschedule.css" rel="stylesheet" type="text/css" />
 <link href="<?php echo Yii::app()->baseUrl.'/css/jqModal.css';?>" rel="stylesheet" type="text/css" />
